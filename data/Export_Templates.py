487fb23d4ea35debcfa9fc578c93710cbebbd30e
alexistrihine-pixel <alexis.trihine@gmail.com>
2026-06-24 15:40:20 +0200

    Add files via upload

---
+++ data/Export_Templates.py
@@ -0,0 +1,11 @@
+# if you didn't install thoses package here is the command : pip install dune-client pandas
+from dune_client.client import DuneClient
+import pandas as pd 
+
+dune = DuneClient("4LMa73t2JMPn128D6NcmJBBHnGwf8bd7")
+query_result = dune.get_latest_result(7748441)  # replace with your query ID
+
+df = pd.DataFrame(query_result.result.rows)
+df.to_csv("privacypools_eth_pool_deposits.csv", index=False) # replace with the file name
+
+print("Done! Saved", len(df), "rows.")
